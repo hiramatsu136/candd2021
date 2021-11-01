1f2e47bd59da9f4a83791f677709f5fe22fc2bf7
CWatanabe210D 渡邉 由紀子 <watanabe210@beerhall.candd.co.jp>
2021-11-01 15:07:06 +0900

    不要なファイル削除

--- watanabe/lifegame/src/router/index.js
+++ watanabe/lifegame/src/router/index.js
@@ -10,14 +10,14 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // { // ページ追加時参考
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
